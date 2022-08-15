521fa8daf793b15c56eb18889c57b280f1c35119
ducminhhh <doducminh141000@gmail.com>
2022-08-15 23:08:10 +0700

    homework7--ducminh

---
+++ index.js
@@ -0,0 +1,13 @@
+let greeting = ('Hello. My name is John.');
+
+let result = greeting.charAt(8)
+console.log(result);
+
+let str1 =('The morning is upon us.');
+let a = str1.slice(11)
+console.log(a);
+
+let str2 = ('Hello');
+let str3 = (' World') ;
+let b = str2.concat(str3);
+console.log(b);
